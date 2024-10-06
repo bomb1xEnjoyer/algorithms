41e3c99b98a4cc7c3b19eecd7b3183fa8256ed25
Almaz Saidov <almaz.saidov.03@bk.ru>
2024-10-06 20:01:22 +0300

    insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,19 @@
+from random import randint\
+
+
+def insertion_sort(lst):
+    n = len(lst)
+    for i in range(1, n):
+        current = lst[i]
+        j = i
+        while j > 0 and lst[j - 1] > current:
+            lst[j] = lst[j - 1]
+            j -= 1
+        lst[j] = current
+    return lst
+
+
+nums = [randint(1, 100) for _ in range(10)]
+print(nums)
+sorted_nums = insertion_sort(nums)
+print(sorted_nums)
