140f556b8d1aeee1843fe83eb2c2504602196a81
Almaz Saidov <almaz.saidov.03@bk.ru>
2024-10-06 19:28:05 +0300

    bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+from random import randint\
+
+
+def bubble_sort(lst):
+    n =  len(lst)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+    return lst
+
+
+nums = [randint(1, 100) for _ in range(10)]
+print(nums)
+sorted_nums = bubble_sort(nums)
+print(sorted_nums)
